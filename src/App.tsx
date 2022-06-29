6ff053458b917f591929bacd6aa809edc121ce3e
ojpro <contact@ojpro.me>
2022-06-29 23:55:53 +0100

    Added TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+    <div className="text-center">
+      <header className="bg-gray-700 min-h-screen text-xl text-white flex flex-col items-center justify-center">
+        <img src={logo} className="w-80" alt="logo" />
+        <p>Hello Vitejs + TailwindCSS + React (TypeScript)!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button
+            type="button"
+            className="bg-blue-400 px-2 py-1.5 rounded"
+            onClick={() => setCount((count) => count + 1)}
+          >
             count is: {count}
           </button>
         </p>
@@ -20,16 +23,25 @@ function App() {
         </p>
         <p>
           <a
-            className="App-link"
+            className="text-blue-400"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
-            className="App-link"
+            className="text-blue-400"
+            href="https://tailwindcss.com/docs/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TailwindCSS
+          </a>
+          {" | "}
+          <a
+            className="text-blue-400"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
@@ -39,7 +51,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
